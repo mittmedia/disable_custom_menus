068e722bb1d2ffe768b98cfa40363d35e65a5999
Fredrik Sundstrom <fredrik.sundstrom@gmail.com>
2012-07-10 16:38:33 +0200

    Disables menu

--- disable_custom_menus.php
+++ disable_custom_menus.php
@@ -1,2 +1,45 @@
 <?php
-#unregister_widget('WP_Nav_Menu_Widget');
+/*
+Plugin Name: Disable Custom Menus
+Plugin URI: https://github.com/mittmedia/disable_custom_menus
+Description:
+Version: 1.0.0
+Author: Fredrik Sundström
+Author URI: https://github.com/fredriksundstrom
+License: MIT
+*/
+
+/*
+Copyright (c) 2012 Fredrik Sundström
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+add_action( 'admin_menu', 'disable_custom_menus' );
+function disable_custom_menus () {
+  // with WP 3.1 and higher
+  if ( function_exists( 'remove_submenu_page' ) ) {
+    remove_submenu_page( 'themes.php', 'nav-menus.php' );
+  } else {
+    unset( $GLOBALS['submenu']['themes.php'][10] );
+  }
+}
